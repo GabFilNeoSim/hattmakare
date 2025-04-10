c7ac5a209adeef7ad74e7506ed271a4d3f6446ca
N30A <neo.ahlander@outlook.com>
2025-04-10 16:19:03 +0200

    Implemented register, login and logout

--- Hattmakare/Controllers/AuthController.cs
+++ Hattmakare/Controllers/AuthController.cs
@@ -9,16 +9,11 @@ namespace Hattmakare.Controllers;
 [Route("auth")]
 public class AuthController : Controller
 {
-    private readonly AppDbContext _context;
     private readonly UserManager<User> _userManager;
     private readonly SignInManager<User> _signInManager;
 
-    public AuthController(
-        AppDbContext context, 
-        UserManager<User> userManager, 
-        SignInManager<User> signInManager)
+    public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
     {
-        _context = context;
         _userManager = userManager;
         _signInManager = signInManager; 
     }
@@ -34,29 +29,69 @@ public class AuthController : Controller
             return View(model);
         }
 
-        var existingUser = await _userManager.FindByEmailAsync(model.Email);
-        if (existingUser != null)
-        {
-            ModelState.AddModelError(string.Empty, "En användare med samma epost finns redan");
-            return View(model);
-        }
-
-        User newUser = new User
+        var newUser = new User
         {
             FirstName = model.FirstName,
             LastName = model.LastName,
+            UserName = model.Email,
             Email = model.Email,
         };
 
         var result = await _userManager.CreateAsync(newUser, model.Password);
         if (!result.Succeeded)
         {
-            ModelState.AddModelError(string.Empty, "Gick ej att skapa kontot");
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
             return View(model);
         }
 
         await _signInManager.SignInAsync(newUser, isPersistent: false);
 
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet("login")]
+    public IActionResult Login() => View(new LoginViewModel());
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        const string errorMessage = "Fel epost eller lösenord";
+        
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null)
+        {
+            ModelState.AddModelError(string.Empty, errorMessage);
+            return View(model);
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(
+            user,
+            model.Password,
+            isPersistent: model.RememberMe,
+            lockoutOnFailure: false
+        );
+
+        if (!result.Succeeded)
+        {
+            ModelState.AddModelError(string.Empty, errorMessage);
+            return View(model);
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
         return RedirectToAction("Index", "Home");
     }
 }
