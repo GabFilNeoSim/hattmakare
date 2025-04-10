7c6da634a94120c25c47de37b13b44637c593e2d
N30A <neo.ahlander@outlook.com>
2025-04-10 16:17:14 +0200

    Added view model for login

--- Hattmakare/Models/Auth/LoginViewModel.cs
+++ Hattmakare/Models/Auth/LoginViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace Hattmakare.Models.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hattmakare.Models.Auth;
 
 public class LoginViewModel
 {
+    [Required(ErrorMessage = "Du måste skriva en epost")]
+    [EmailAddress(ErrorMessage = "Fel format på eposten")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
 
+    [Required(ErrorMessage = "Skriv in ett lösenord")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; } = false;
 }
