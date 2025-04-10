7ce6b58c3abaf894862ec8c99a400deeec36a0f7
N30A <95051018+N30A@users.noreply.github.com>
2025-04-10 15:30:18 +0200

    Added view models for registration and login

---
+++ Hattmakare/Models/Auth/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hattmakare.Models.Auth;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Du måste skriva ett förnamn")]
+    [RegularExpression("^[a-zA-ZåäöÅÄÖ]+$", ErrorMessage = "Endast bokstäver är tillåtna")]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Du måste skriva ett efternamn")]
+    [RegularExpression("^[a-zA-ZåäöÅÄÖ]+$", ErrorMessage = "Endast bokstäver är tillåtna")]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Du måste skriva en epost")]
+    [EmailAddress(ErrorMessage = "Fel format på eposten")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Skriv in ett lösenord")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Skriv in ett bekräftelselösenord")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Båda lösenorden måste matcha")]
+    public string ConfirmPassword { get; set; }
+}
