28907727b58f0ac4c15337764b278ef6e0e5cd62
N30A <95051018+N30A@users.noreply.github.com>
2025-04-10 10:23:16 +0200

    Add CustomerController

---
+++ Hattmakare/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hattmakare.Controllers;
+
+[Route("customers")]
+public class CustomerController : Controller
+{
+    [HttpGet] // Show customer page
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("add")]
+    public IActionResult AddCustomer(object customer)
+    {
+        throw new NotImplementedException();
+    }
+}
