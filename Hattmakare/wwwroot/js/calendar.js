1db620d86ce41bfccde1ee6b8294a27c533c73d7
Filip Siri <filipsiri03@hotmail.com>
2025-04-11 13:41:42 +0200

    Progress on calendar

---
+++ Hattmakare/wwwroot/js/calendar.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        firstDay: 1,
+        locale: 'sv',
+        events: '/Home/PopulateCalendar',
+        eventClick: function (info) {
+            const date = info.event.startStr;
+            $.get('/Home/PopulateCalendarPopUp', { date: date }, function (data) {
+                $('#orderModalBody').html(data);
+                $('#orderModal').modal('show');
+            });
+        }
+    });
+
+    calendar.render();
+})
