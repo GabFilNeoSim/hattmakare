72eb26477ad16892cf69fa2c97a0a4917ebaa8fe
Filip Siri <filipsiri03@hotmail.com>
2025-04-14 09:33:39 +0200

    Comment out calendar

--- Hattmakare/wwwroot/js/calendar.js
+++ Hattmakare/wwwroot/js/calendar.js
@@ -1,22 +1,22 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
+﻿//document.addEventListener('DOMContentLoaded', function () {
+//    var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        firstDay: 1,
-        locale: 'sv',
-        buttonText: {
-            today: 'Idag'
-        },
-        events: '/Home/PopulateCalendar',
-        eventClick: function (info) {
-            const date = info.event.startStr;
-            $.get('/Home/PopulateCalendarPopUp', { date: date }, function (data) {
-                $('#orderModalBody').html(data);
-                $('#orderModal').modal('show');
-            });
-        }
-    });
+//    var calendar = new FullCalendar.Calendar(calendarEl, {
+//        initialView: 'dayGridMonth',
+//        firstDay: 1,
+//        locale: 'sv',
+//        buttonText: {
+//            today: 'Idag'
+//        },
+//        events: '/Home/PopulateCalendar',
+//        eventClick: function (info) {
+//            const date = info.event.startStr;
+//            $.get('/Home/PopulateCalendarPopUp', { date: date }, function (data) {
+//                $('#orderModalBody').html(data);
+//                $('#orderModal').modal('show');
+//            });
+//        }
+//    });
 
-    calendar.render();
-})
+//    calendar.render();
+//})
