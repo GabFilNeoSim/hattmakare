3c067c48a4126f74fdc4ac0f8598d367c7c57663
s-persson <simon20396@gmail.com>
2025-04-14 09:37:07 +0200

    WIP new order hats

---
+++ Hattmakare/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Hattmakare.Models.Hat;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hattmakare.Controllers
+{
+  [Route("order")]
+  public class OrderController : Controller 
+  {
+    [HttpGet("new/hats")]
+    public async Task<IActionResult> Hats()
+    {
+      var hats = new HatsViewModel();
+      hats.Hats = new List<HatViewModel>
+        {
+            new HatViewModel { Name = "Hat 1", ImageUrl = "500x500_placeholder.png" },
+            new HatViewModel { Name = "Hat 2", ImageUrl = "500x500_placeholder.png" },
+            new HatViewModel { Name = "Hat 3", ImageUrl = "500x500_placeholder.png" },
+            new HatViewModel { Name = "Hat 4", ImageUrl = "500x500_placeholder.png" },
+            new HatViewModel { Name = "Hat 5", ImageUrl = "500x500_placeholder.png" },
+            new HatViewModel { Name = "Hat 6", ImageUrl = "500x500_placeholder.png" }
+        };
+      return View(hats);
+    }
+  }
+}
--- Hattmakare/wwwroot/js/hat.js
+++ Hattmakare/wwwroot/js/hat.js
@@ -1,3 +1,26 @@
+const shoppingCart = {};
+
+document.querySelectorAll('.hatItem').forEach(hatItem => {
+    const plusBtn = hatItem.querySelector('.count-add');
+    const minusBtn = hatItem.querySelector('.count-remove');
+    const counterP = hatItem.querySelector('.count');
+    const hatName = hatItem.getAttribute('data-name');
+
+    plusBtn.addEventListener('click', () => {
+        shoppingCart[hatName] = (shoppingCart[hatName] || 0) + 1;
+        counterP.textContent = shoppingCart[hatName];
+        console.log(shoppingCart);
+    });
+
+    minusBtn.addEventListener('click', () => {
+        if (shoppingCart[hatName] > 0) {
+            shoppingCart[hatName] -= 1;
+            counterP.textContent = shoppingCart[hatName];
+            console.log(shoppingCart);
+        }
+    });
+});
+
 $(".editHat").on("click", function (e) {
     console.log("Clicked edit")
     console.log($(this).data("item"))
