6ca9aa666ada64032a60e672a78f6dd3a3a2f913
s-persson <simon20396@gmail.com>
2025-04-16 17:23:23 +0200

    Choose hats + shopping cart

--- Hattmakare/wwwroot/js/hat.js
+++ Hattmakare/wwwroot/js/hat.js
@@ -1,38 +1,3 @@
-const cart = {};
-
-document.querySelectorAll('.hatItem').forEach(hatItem => {
-    const plusBtn = hatItem.querySelector('.count-add');
-    const minusBtn = hatItem.querySelector('.count-remove');
-    const counterElement = hatItem.querySelector('.count');
-    const id = hatItem.getAttribute('data-id');
-
-    plusBtn.addEventListener('click', () => {
-        cart[id] = (cart[id] || 0) + 1;
-        counterElement.textContent = cart[id];
-        console.log(cart);
-        updateCartItems();
-    });
-
-    minusBtn.addEventListener('click', () => {
-        if (cart[id] > 0) {
-            cart[id] -= 1;
-            counterElement.textContent = cart[id];
-            console.log(cart);
-        }
-    });
-});
-
-function updateCartItems() {
-    console.log("Updated cart")
-    const cartEl = $("#cart")
-    cart.forEach(function(k, v) {
-        let html = `
-        <p>${v} </p>
-        `;
-        cartEl.append(html)
-    })
-}
-
 $(".editHat").on("click", function (e) {
     console.log("Clicked edit")
     console.log($(this).data("item"))
