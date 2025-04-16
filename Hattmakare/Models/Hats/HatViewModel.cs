a5ad9d6fd94a3a23004b89cd4442388ab7435185
LinaRosen <linros0102@gmail.com>
2025-04-16 13:28:00 +0200

    Lägga till antal, pris och storlek på hattar. Samma sak med att ändra antal, pris och storlek på hattar.

--- Hattmakare/Models/Hats/HatViewModel.cs
+++ Hattmakare/Models/Hats/HatViewModel.cs
@@ -11,5 +11,8 @@ namespace Hattmakare.Models.Hats
         public string Name { get; set; }
         public string ImageUrl { get; set; }
         public bool IsDeleted { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+        public int InStock { get; set; }
     }
 }
