4f72c9c78182d29cad50fdd083d75a7db24329c1
amandaperbro <amanda.perbro@hotmail.com>
2025-04-21 16:06:17 +0200

    Början på fraktsedel

---
+++ Hattmakare/Models/Order/WayBillViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Hattmakare.Data.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Hattmakare.Models.Order;
+
+
+public class WayBilViewModel
+{
+    public string address { get; set; }
+    public User sender { get; set; }
+    public Data.Entities.Customer reciver { get; set; }
+    public int Export { get; set; }
+    public decimal price { get; set; }
+    public int weight { get; set; }
+
+    public decimal tax => price * 0.25m;
+
+    public decimal totalPrice => price + tax;
+
+    public Data.Entities.Order order { get; set; }
+    public int orderNumber { get; set; }
+
+    public List<OrderHat> OrderHats { get; set; } = new();
+
+    //public List<User> Users { get; set; }
+
+    //public List<Hattmakare.Data.Entities.Customer> customers { get; set; }
+
+
+}
+
+
