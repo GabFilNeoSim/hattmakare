b0096572fe57d5d99f1c699c40a17d271ed4b7c1
N30A <neo.ahlander@outlook.com>
2025-04-22 20:37:17 +0200

    Added endpoint for editing user, order by role, small fixes

--- Hattmakare/Controllers/UserController.cs
+++ Hattmakare/Controllers/UserController.cs
@@ -19,32 +19,34 @@ public class UserController : Controller
         _userManager = userManager;
     }
 
+    [HttpGet]
     public async Task<IActionResult> Index()
-    {   
+    {
         var model = new UserIndexViewModel
         {
             Users = await _context.Users
-        .GroupJoin(_context.UserRoles,
-            user => user.Id,
-            userRole => userRole.UserId,
-            (user, userRoles) => new { user, userRoles })
-        .SelectMany(
-            x => x.userRoles.DefaultIfEmpty(), // Vänster join
-            (x, userRole) => new { x.user, userRole })
-        .GroupJoin(_context.Roles,
-            combined => combined.userRole.RoleId,
-            role => role.Id,
-            (combined, roles) => new { combined.user, roles })
-        .SelectMany(
-            x => x.roles.DefaultIfEmpty(), // Vänster join
-            (x, role) => new UserViewModel
-            {
-                FirstName = x.user.FirstName,
-                LastName = x.user.LastName,
-                Email = x.user.Email!,
-                Role = role != null ? role.Name : "Ingen roll" // Om ingen roll finns
-            })
-        .ToListAsync()
+            .GroupJoin(_context.UserRoles,
+                user => user.Id,
+                userRole => userRole.UserId,
+                (user, userRoles) => new { user, userRoles })
+            .SelectMany(
+                x => x.userRoles.DefaultIfEmpty(),
+                (x, userRole) => new { x.user, userRole })
+            .GroupJoin(_context.Roles,
+                combined => combined.userRole.RoleId,
+                role => role.Id,
+                (combined, roles) => new { combined.user, roles })
+            .SelectMany(
+                x => x.roles.DefaultIfEmpty(),
+                (x, role) => new UserViewModel
+                {
+                    Id = x.user.Id,
+                    FirstName = x.user.FirstName,
+                    LastName = x.user.LastName,
+                    Email = x.user.Email!,
+                    Role = role != null ? role.Name : "Ingen roll"
+                }).OrderBy(x => x.Role)
+            .ToListAsync()
         };
 
         return View(model);
@@ -84,20 +86,74 @@ public class UserController : Controller
             await _userManager.AddToRoleAsync(newUser, "Admin");
         }
 
-        return RedirectToAction("Index", "User");
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> EditUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        var model = new EditUserViewModel
+        {
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email!,
+            IsAdmin = await _userManager.IsInRoleAsync(user, "Admin")
+        };
+
+        return View(model);
+    }
+
+    [HttpPost("{id}")]
+    public async Task<IActionResult> EditUser(string id, EditUserViewModel model)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        if (!ModelState.IsValid)
+        {   
+            return View(nameof(EditUser), model);
+        }
+
+        user.FirstName = model.FirstName;
+        user.LastName = model.LastName;
+        user.Email = model.Email;
+        user.UserName = model.Email;
+
+        switch (model.IsAdmin) {
+            case true:
+                await _userManager.AddToRoleAsync(user, "Admin");
+                break;
+            case false:
+                await _userManager.RemoveFromRoleAsync(user, "Admin");
+                break;
+        }
+
+        await _userManager.UpdateAsync(user);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
     }
 
     [HttpPost("{id}/remove")]
     public async Task<IActionResult> RemoveUser(string id)
-    {   
+    {
         var user = await _userManager.FindByIdAsync(id);
         if (user == null)
         {
-            return RedirectToAction("Index", "User");
+            return RedirectToAction(nameof(Index));
         }
 
         await _userManager.DeleteAsync(user);
 
-        return RedirectToAction("Index", "User");
+        return RedirectToAction(nameof(Index));
     }
 }
