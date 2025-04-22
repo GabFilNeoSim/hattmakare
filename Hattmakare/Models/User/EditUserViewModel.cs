dc59db2aad8fc039f3705e7c90048925ea5865fc
N30A <neo.ahlander@outlook.com>
2025-04-22 20:34:26 +0200

    cshtml and VM for editing user

---
+++ Hattmakare/Models/User/EditUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hattmakare.Models.User;
+
+public class EditUserViewModel
+{
+    public string Id { get; set; }
+
+    [Required(ErrorMessage = "Skriv in ett förnamn")]
+    [RegularExpression("^[a-zA-ZåäöÅÄÖ]+$", ErrorMessage = "Endast bokstäver är tillåtna")]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Skriv in ett efternamn")]
+    [RegularExpression("^[a-zA-ZåäöÅÄÖ]+$", ErrorMessage = "Endast bokstäver är tillåtna")]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Skriv in en epost")]
+    [EmailAddress(ErrorMessage = "Fel format på eposten")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    public bool IsAdmin { get; set; } = false;
+}
