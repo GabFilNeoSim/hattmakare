1a0247a40cbb84a3baaab922ecfb9d8bed27ce4f
N30A <95051018+N30A@users.noreply.github.com>
2025-04-23 10:37:54 +0200

    Replaced confirm popup

--- Hattmakare/wwwroot/js/customer.js
+++ Hattmakare/wwwroot/js/customer.js
@@ -1,8 +1,4 @@
-﻿function confirmDelete() {
-    return confirm("Do you want to delete this costumer from the register?");
-}
-
-//landskod för telefonnummer 
+﻿//landskod för telefonnummer 
 document.addEventListener("DOMContentLoaded", function () {
     const visibleInput = document.querySelector("#visiblePhone");
     const hiddenInput = document.querySelector("#fullPhone");
