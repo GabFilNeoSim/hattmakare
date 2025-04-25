6706573ab5410221bb6050434d81ee8e4593584c
dolken9 <gabriel.dolk@hotmail.com>
2025-04-25 09:11:15 +0200

    Tog bort två fält i viewmodel

--- Hattmakare/Models/Statistics/StatisticsViewModel.cs
+++ Hattmakare/Models/Statistics/StatisticsViewModel.cs
@@ -7,16 +7,11 @@ namespace Hattmakare.Models.Statistics
     public class StatisticsViewModel
     {
         public string? CustomerId { get; set; }
-        public string? HatId { get; set; }
-        public List<string> HatNames { get; set; }
-        public List<int> Sales { get; set; }
-        
+        public string? HatId { get; set; }       
         public List<string> DailyLabels { get; set; } 
         public List<int> DailySales { get; set; }
         public List<int> QuarterlySales { get; set; }
         public List<int> MonthlySales { get; set; }
-
-
         public List<SelectListItem> Hats { get; set; }
         public List<SelectListItem> Customers { get; set; }
         public List<HatsViewModel> hatsViewModels { get; set; }
