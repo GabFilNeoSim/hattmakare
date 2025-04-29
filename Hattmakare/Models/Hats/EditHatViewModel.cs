4d275474d57a18be6e70b77e56d04fe1913ad7ad
dolken9 <gabriel.dolk@hotmail.com>
2025-04-29 17:02:48 +0200

    Edit

--- Hattmakare/Models/Hats/EditHatViewModel.cs
+++ Hattmakare/Models/Hats/EditHatViewModel.cs
@@ -33,6 +33,8 @@ public class EditHatViewModel
     [Required(ErrorMessage = "Skriv in ett antal i lager")]
     public int Quantity { get; set; }
 
+    public string? Comment { get; set; }
+
     public IFormFile? Image { get; set; }
 
     public bool IsSpecial { get; set; } = false;
